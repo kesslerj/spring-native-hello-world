bebc0bc113ab1787104cf5ba15121de6fa5d5428
Jonas Keßler <jonas.kessler@aclue.de>
2023-09-19 20:48:10 +0200

    smaller test app

--- src/main/java/de/aclue/springnativehelloworld/persistence/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package de.aclue.springnativehelloworld.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
